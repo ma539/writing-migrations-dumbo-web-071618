da0af43d8d7b3a919dbc3d6f4d54b671eda151dd
ma539@cornell.edu <ma539@cornell.edu>
2018-07-31 03:16:06 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change 
-    create_table :artists do |t|
+    create_table :students do |t|
       t.string :name 
-      t.string :genre 
-      t.string 
+    end
 end
